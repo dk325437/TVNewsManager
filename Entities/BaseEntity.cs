c9b9649fd81931e9b728a1c6d5bd6398a0085854
dk325437 <pndkhoa459974@gmail.com>
2025-09-22 21:53:05 +0700

    Update BaseEntity.cs

--- Entities/BaseEntity.cs
+++ Entities/BaseEntity.cs
@@ -3,11 +3,15 @@ using TVNewsManager.Collections;
 
 namespace TVNewsManager.Entities
 {
+    // Lớp cơ sở trừu tượng cho các thực thể trong hệ thống
     [Serializable]
     public abstract class BaseEntity
     {
+        // Mỗi thực thể có một Id duy nhất (string)
         public string Id { get; set; }
 
+        // Constructor bảo vệ: khi tạo mới đối tượng thì tự sinh Id (dùng Guid)
+        // Guid.NewGuid().ToString() đảm bảo Id là duy nhất toàn cục
         protected BaseEntity()
         {
             Id = Guid.NewGuid().ToString();
