4ba85c4be4888066b157d8c61c0c5332552251c4
dk325437 <pndkhoa459974@gmail.com>
2025-09-22 21:57:07 +0700

    Update BienTapVien.cs

--- Entities/BienTapVien.cs
+++ Entities/BienTapVien.cs
@@ -2,13 +2,19 @@
 
 namespace TVNewsManager.Entities
 {
+    // Lớp Biên tập viên kế thừa từ lớp NhanSu
+    // Có thể đại diện cho nhân sự cụ thể trong hệ thống
     [Serializable]
     public class BienTapVien : NhanSu
     {
-        public BienTapVien() { } // constructor mặc định
+        // Constructor mặc định (không tham số)
+        public BienTapVien() { }
 
+        // Constructor có tham số: khởi tạo Biên tập viên với tên, email và số điện thoại
+        // Gọi constructor của lớp cha (NhanSu) thông qua từ khóa base
         public BienTapVien(string ten, string e, string sdt) : base(ten, e, sdt) { }
 
+        // Ghi đè phương thức ToString() để hiển thị thông tin biên tập viên
         public override string ToString()
         {
             return $"Biên tập viên: {HoTen}";
